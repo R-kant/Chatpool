f4889fb0b67f05428a2c4583e89c028c3d2cc1ef
R-kant <37539830+R-kant@users.noreply.github.com>
2018-12-25 18:36:17 +0530

    procees.env.ip

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ function isLoggedin(req,res,next){
 	}
 	res.redirect('/login');
 }
-var server=app.listen(3000);
+var server=app.listen(process.env.PORT,process.env.IP);
 
 var io = socket(server);
 var users=[];
